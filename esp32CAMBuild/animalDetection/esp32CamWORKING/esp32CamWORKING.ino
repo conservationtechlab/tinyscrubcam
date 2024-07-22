32efc16a444055be1e8eb2121b2c2f96c4961b09
Venz Burgos <venz.burgos@yahoo.com>
2024-07-22 15:27:14 -0700

    file not uploading

--- esp32CAMBuild/animalDetection/esp32CamWORKING/esp32CamWORKING.ino
+++
@@ -1,6 +0,0 @@
-// 1 save image only when machine learning algorithm sees the object of interest
-// 0 save every image a machine learning inference is made on, meaning every frame checked in the 10s window
-#define saveDetectionImage 1
-
-// File for Edge Impulse Model
-#include <UCSD-project-1_inferencing.h>
