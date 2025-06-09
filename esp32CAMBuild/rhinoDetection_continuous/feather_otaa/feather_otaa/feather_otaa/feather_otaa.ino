fe81550902d1769affec98b6a4cd42d524c5e7a0
Conor-StJohn <crstjohn.707@gmail.com>
2025-06-09 13:43:03 -0700

    Update feather_otaa.ino
    Working Version of Rhino-Test using OTAA

--- esp32CAMBuild/rhinoDetection_continuous/feather_otaa/feather_otaa/feather_otaa/feather_otaa.ino
+++ esp32CAMBuild/rhinoDetection_continuous/feather_otaa/feather_otaa/feather_otaa/feather_otaa.ino
@@ -53,13 +53,13 @@ static const u1_t PROGMEM APPEUI[8]= { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 
 // This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8]= { 0xF2, 0xD7, 0x9D, 0xAA, 0xEC, 0xC5, 0x4B, 0xC5 };
+static const u1_t PROGMEM DEVEUI[8]= { 0xF4, 0x69, 0xA3, 0x84, 0xA6, 0xDB, 0x0E, 0x59  };
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from the TTN console can be copied as-is.
-static const u1_t PROGMEM APPKEY[16] = {  0xB5, 0xB6, 0xB8, 0xB3, 0xE3, 0x43, 0x34, 0x54, 0xBC, 0x87, 0xC3, 0x69, 0x61, 0xB1, 0x77, 0xD1 };
+static const u1_t PROGMEM APPKEY[16] = {  0xDD, 0x1E, 0x23, 0x79, 0x45, 0x3E, 0x5A, 0xB9, 0x84, 0xF9, 0xC3, 0x78, 0x6F, 0xE1, 0x58, 0xDB };
 void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
 
 static uint8_t mydata[] = "Rhinos";
@@ -128,9 +128,7 @@ void onEvent (ev_t ev) {
         case EV_JOINING:
             Serial.println(F("EV_JOINING"));
                   //blink if completed
-    digitalWrite(13, HIGH);
-    delay(10000);
-    digitalWrite(13, LOW);
+
             break;
         case EV_JOINED:
             Serial.println(F("EV_JOINED"));
@@ -253,9 +251,9 @@ void do_send(osjob_t* j){
 
 void setup() {
 digitalWrite(13, HIGH);
-   delay(5000); //change back to 5000 if need be
+   delay(15000); //change back to 5000 if need be
 
-  while (! Serial);
+ while (!Serial && millis() < 5000);  // wait up to 5 seconds for Serial
     Serial.begin(9600); 
     delay(100);
     Serial1.begin(115200);     // ALLOWS RX AND TX TO BE ABLE TO READ SERIAL1 DATA BY 
@@ -287,76 +285,76 @@ digitalWrite(13, HIGH);
 
     LMIC_setLinkCheckMode(0);
     // TTN uses SF9 for its RX2 window.
-  LMIC.dn2Dr = DR_SF9;
+    //LMIC.dn2Dr = DR_SF9;
     LMIC_setDrTxpow(DR_SF7,14);
-  //  LMIC_selectSubBand(1);
-  // TTN uses SF9 for its RX2 window.
-    //LMIC.dn2Dr = DR_SF9; //Consider changing this
+    LMIC_selectSubBand(0);
+
 
    digitalWrite(13, LOW);
  
 
-do  {
+/*do  {
 String receivedString = Serial1.readStringUntil('\n'); // Read until newline character
        Serial.print("Received: ");
          Serial.println(receivedString); // print the received line
-    
-    digitalWrite(13, HIGH);
-    delay(2000);
-  digitalWrite(13, LOW);
-   delay(2000);
-   digitalWrite(13, HIGH);
- delay(2000);
-   digitalWrite(13, LOW);
 
           if (receivedString.length() < 15) {
               receivedString.getBytes(mydata, receivedString.length() + 1); // +1 to include the null terminator
+                 do_send(&sendjob);
           } else {
               Serial.println("Out of bounds");
               receivedString = "ERR: Check Cam";
           }
      
 } while (Serial1.available() > 0);
-    // while (1) {
-     // if (Serial1.available() > 0) { // Check if data is available to read
-         // String receivedString = Serial1.readStringUntil('\n'); // Read until newline character
-         // Serial.print("Received: ");
-         // Serial.println(receivedString); // print the received line
-          
-         //digitalWrite(13, HIGH);
-         //delay(2000);
-        //digitalWrite(13, LOW);
-       // delay(2000);
-       // digitalWrite(13, HIGH);
-       //  delay(2000);
-       // digitalWrite(13, LOW);
 
-         // if (receivedString.length() < 15) {
-           //   receivedString.getBytes(mydata, receivedString.length() + 1); // +1 to include the null terminator
-         // } else {
-         //     Serial.println("Out of bounds");
-       //       receivedString = "ERR: Check Cam";
-      //    }
-     //    break; //original
-  //    }
-  // break; //actually breaks out endless loop
-//}
+    while (1) {
+      if (Serial1.available() > 0) { // Check if data is available to read
+          String receivedString = Serial1.readStringUntil('\n'); // Read until newline character
+          Serial.print("Received: ");
+          Serial.println(receivedString); // print the received line
+          
+         digitalWrite(13, HIGH);
+         delay(2000);
+        digitalWrite(13, LOW);
+        delay(2000);
+       digitalWrite(13, HIGH);
+        delay(2000);
+        digitalWrite(13, LOW);
+
+         if (receivedString.length() < 15) {
+              receivedString.getBytes(mydata, receivedString.length() + 1); // +1 to include the null terminator
+         } else {
+             Serial.println("Out of bounds");
+             receivedString = "ERR: Check Cam";
+        }
+        break; //original
+     }
+   break; //actually breaks out endless loop
+}
 
     // Start job (sending automatically starts OTAA too)
-    do_send(&sendjob);
-    //blink twice if completed
-    digitalWrite(13, HIGH);
-    delay(2000);
-   digitalWrite(13, LOW);
-   delay(2000);
-   digitalWrite(13, HIGH);
- delay(2000);
-   digitalWrite(13, LOW);
- 
+   
+    */
+
 }
 
 void loop() {
     os_runloop_once();
-}
 
+    while (Serial1.available()) { // Check if data is available to read
+String receivedString = Serial1.readStringUntil('\n'); // Read until newline character
+       Serial.print("Received: ");
+         Serial.println(receivedString); // print the received line
+
+          if (receivedString.length() < 15) {
+              receivedString.getBytes(mydata, receivedString.length() + 1); // +1 to include the null terminator
+                 do_send(&sendjob);
+          } else {
+              Serial.println("Out of bounds");
+              receivedString = "ERR: Check Cam";
+          }
+    }
+
+}
 
