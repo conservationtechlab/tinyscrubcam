e5273541f0c8a321e339300aa12ad865fe2c5b6a
Conor St John <crstjohn.707@gmail.com>
2025-06-30 09:13:01 -0700

    Working Version of Bidirectional Communication for Feather M0

--- esp32CAMBuild/rhinoDetection_continuous/FEATHER_M0_BIDIRECTIONAL_COMMUNICATION/FEATHER_M0_BIDIRECTIONAL_COMMUNICATION/FEATHER_M0_BIDIRECTIONAL_COMMUNICATION.ino
+++ esp32CAMBuild/rhinoDetection_continuous/FEATHER_M0_BIDIRECTIONAL_COMMUNICATION/FEATHER_M0_BIDIRECTIONAL_COMMUNICATION/FEATHER_M0_BIDIRECTIONAL_COMMUNICATION.ino
@@ -41,17 +41,17 @@
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
 // 0x70.
-static const u1_t PROGMEM APPEUI[8]= { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const u1_t PROGMEM APPEUI[8]= {FILL ME IN}; //unless shown otherwise fill this in as 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8); }
 
 // This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8]= {0xA0,  0x0F,  0x18,  0xA3,  0x3C,  0xBF,  0x8C, 0xD5};
+static const u1_t PROGMEM DEVEUI[8]= {FILL ME IN};
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8); }
 
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from the TTN console can be copied as-is.
-static const u1_t PROGMEM APPKEY[16] = {0x00, 0x4A, 0x55, 0x9B, 0x16, 0xF3, 0x3E, 0x68, 0x21, 0xBA, 0x45, 0xF2, 0xB8, 0xAB, 0xA0, 0xCA };
+static const u1_t PROGMEM APPKEY[16] = {FILL ME IN };
 void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16); }
 
 #define MAX_LENGTH 30
